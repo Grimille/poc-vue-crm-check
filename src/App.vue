5d446fbcf56110dc9d9c3d2aa5514cad260b248f
Mickaël Dhainaut <mickael@popsell.com>
2022-11-22 17:30:13 +0100

    🔥 Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="container">
+        <div class="toolbar">
+          <button v-if="mode" type="button" @click="selectAll">Tout séléctionner ({{ contacts.length }})</button>
+          <button v-else type="button" @click="deselectAll">Tout déséléctionner ({{ contacts.length }})</button>
+        </div>
+        <div class="card" v-for="(contact, i) in contacts" :key="'lol' + i">
+          <input type="checkbox" :checked="contact.checked">
+          <img class="avatar" :src="contact.image" alt="">
+          <div class="card-contact">
+            <span>{{ contact.fullName }}</span>
+            <span>{{ contact.email }}</span>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import fake from '@/assets/fake.json';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      contacts: [],
+      mode: true
+    }
+  },
+  methods: {
+    selectAll() {
+      this.contacts.map((c) => c.checked = true);
+      this.mode = false;
+    },
+    deselectAll() {
+      this.contacts.map((c) => c.checked = false);
+      this.mode = true;
+    }
+  },
+  mounted() {
+    fake.forEach((c) => {
+      this.contacts.push({ ...c, checked: false});
+    })
   }
 }
 </script>
@@ -21,8 +51,35 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container {
+  width: 90%;
+  margin: 0 auto;
+}
+
+.toolbox {
+
+}
+
+.card {
+  display: flex;
+  margin: 1em 0;
+  padding: 1em;
+  background: rgba(0,0, 0, 0.1);
+}
+
+.card-contact {
+  display: flex;
+  flex-flow: column nowrap;
+}
+
+.avatar {
+  width: 48px;
+  height:48px;
+  border-radius: 5px;
+  margin-right: 1em;
+  margin-left: 1em;
 }
 </style>
